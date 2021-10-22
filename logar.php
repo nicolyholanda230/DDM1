795187397ef2916e5ba3c829bbfc1ec95b961ff4
Nicoly Holanda Raposo Ferreira <64547020+nicolyholanda230@users.noreply.github.com>
2021-10-22 16:36:18 -0300

    scripts php e arquivo AIA - Inicial

---
+++ logar.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'conexao.php';
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+//executar pesquisa no bd
+$sql = $dbcon->query("SELECT * FROM tblogin WHERE email='$email' AND senha='$senha'");
+
+if(mysqli_num_rows($sql)>0){
+    echo "login_ok";
+}else{
+    echo "login_er";
+}
+
+?>
